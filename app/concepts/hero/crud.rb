d69955edb2207a3c05a8c80620a08f77e53e3355
Jamie Macey <jamie.git@tracefunc.com>
2015-01-11 13:31:03 -0800

    initial Hero validations

--- app/concepts/hero/crud.rb
+++ app/concepts/hero/crud.rb
@@ -1,7 +1,28 @@
 class Hero < ActiveRecord::Base
   class Create < Trailblazer::Operation
+    attr_reader :model
+
+    contract do
+      property :name, validates: {presence: true}
+      property :archetype, validates: {presence: true}
+      property :speed
+      property :health
+      property :stamina
+      property :defense
+      property :might
+      property :knowledge
+      property :willpower
+      property :awareness
+      property :hero_ability
+      property :heroic_feat
+    end
+
     def process(params)
-      Hero.create(params[:hero])
+      @model = Hero.new
+
+      validate(params[:hero], @model) do |f|
+        f.save
+      end
     end
   end
 end
--- test/concepts/hero/crud_test.rb
+++ test/concepts/hero/crud_test.rb
@@ -4,11 +4,24 @@ class HeroCrudTest < MiniTest::Spec
   describe "Create" do
     it "persists valid" do
       hero = Hero::Create[hero:
-        {name: "Alys Rayne"}
-      ]
+        { name: "Alys Rayne",
+          archetype: "Warrior"
+        }
+      ].model
 
       hero.persisted?.must_equal true
       hero.name.must_equal "Alys Rayne"
+      hero.archetype.must_equal "Warrior"
+    end
+
+    it "invalid" do
+      result, operation = Hero::Create.run(hero:
+        {name: "Alys Rayne"}
+      )
+
+      result.must_equal false
+      operation.model.persisted?.must_equal false
+      operation.contract.errors.to_s.must_equal "{:archetype=>[\"can't be blank\"]}"
     end
   end
 end
